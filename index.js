4485ec68f82287b9a0976ee204e7cabbf51ecfee
tnjensen <thomasnorupjensen@gmail.com>
2023-06-29 18:03:23 +0200

    Refactored index

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const dbConnect = require('./dbConnect');
@@ -13,36 +11,28 @@ app.use(cors());
 
 dbConnect();
 
-/* mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}, () =>{
-    console.log('Connected to MongoDB');
-})
- */
 //middleware
 app.use(express.json()); //parser
 app.use(morgan("common"));
 
 //Cors fix
-/* app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://mern-auth-mkbw.onrender.com/login");
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
     next();
-}); */
+});
 
 app.use("/api/users", userRoute);
 app.use("/auth", authRoute);
 
 //test
-/* app.get('/', (req,res) => {
+app.get('/', (req,res) => {
     res.send('Welcome to the home page');
 });
 app.get('/users', (req,res) => {
     res.send('Welcome to the user page');
-}); */
-
-/* app.listen(8800, () =>{
-    console.log('Backend server is running');
-}); */
+});
 
 app.listen(PORT, () =>{
     console.log(`Backend server is running on port ${PORT}`);
